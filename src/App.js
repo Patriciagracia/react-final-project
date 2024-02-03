6ed91b85f78b556d3a90ec051bbf5b9571ed5a7a
Patriciagracia <patriciagraciaherrero@gmail.com>
2024-02-03 15:25:49 +0100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App Hello</div>;
+  return (
+    <div className="App">
+      <footer>
+        {" "}
+        This project is created by Patricia Gracia.{" "}
+        <a
+          href="https://github.com/Patriciagracia/react-final-project"
+          target="_blank"
+        >
+          {" "}
+          Open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
